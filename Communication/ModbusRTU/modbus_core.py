e72e899b430e679c2b990e1050bdf0128ca41bf6
MuC <nam1811.hust@gmail.com>
2021-07-08 11:04:55 +0700

    add req

--- Communication/ModbusRTU/modbus_core.py
+++ Communication/ModbusRTU/modbus_core.py
@@ -38,52 +38,75 @@ class ModbusApp(Ui_MainWindow):
         self.stopReading.clicked.connect(self.stop_reading)
         self.set_random()
 
+    def popup_msg(self, msg, source_msg='', type_msg='warning'):
+        if type_msg == 'warning':
+            pass
+        elif type_msg == 'error':
+            pass
+        elif type_msg == 'infor':
+            pass
+        print(msg)
+        pass
+
     def read_csv_data(self, table_name):
-        data = pd.read_csv(f'backup/{table_name}.csv')
-        if table_name == 'setpoints':
-            self.name_set = list(data['name'])
-            self.type_set = list(data['type'])
-            self.values_set = list(data['value'])
-            self.address_set = list(data['address'])
-        elif table_name == 'trackdevice':
-            self.name_track = list(data['name'])
-            self.type_track = list(data['type'])
-            self.address_track = list(data['address'])
-        print(f'read from {table_name} done')
+        try:
+            data = pd.read_csv(f'backup/{table_name}.csv')
+            if table_name == 'setpoints':
+                self.name_set = list(data['name'])
+                self.type_set = list(data['type'])
+                self.values_set = list(data['value'])
+                self.address_set = list(data['address'])
+            elif table_name == 'trackdevice':
+                self.name_track = list(data['name'])
+                self.type_track = list(data['type'])
+                self.address_track = list(data['address'])
+            print(f'read from {table_name} done')
+        except Exception as e:
+            self.show_error(e)
 
     # setpoints blocks
     def update_set_value(self):
-        table = self.setValueTable
-        nrows = table.rowCount()
-        for i in range(nrows):
-            if not isinstance(table.item(i, 0), type(None)):
-                self.type_set[i] = table.item(i, 0).text()
-                self.values_set[i] = table.item(i, 1).text()
-        pass
+        try:
+            table = self.setValueTable
+            nrows = table.rowCount()
+            for i in range(nrows):
+                if not isinstance(table.item(i, 0), type(None)):
+                    self.type_set[i] = table.item(i, 0).text()
+                    self.values_set[i] = table.item(i, 1).text()
+        except Exception as e:
+            self.show_error(e)
 
     def reset_set_table(self):
-        _translate = QtCore.QCoreApplication.translate
-        self.read_csv_data('setpoints')
-        for i in range(len(self.name_set)):
-            table = self.setValueTable
-            table.verticalHeaderItem(i).setText(_translate("MainWindow", self.name_set[i]))  # set name
-            table.setItem(i, 0, QTableWidgetItem(f"{self.type_set[i]}"))
-            table.setItem(i, 1, QTableWidgetItem(f"{self.values_set[i]}"))
-
+        try:
+            _translate = QtCore.QCoreApplication.translate
+            self.read_csv_data('setpoints')
+            for i in range(len(self.name_set)):
+                table = self.setValueTable
+                table.verticalHeaderItem(i).setText(_translate("MainWindow", self.name_set[i]))  # set name
+                table.setItem(i, 0, QTableWidgetItem(f"{self.type_set[i]}"))
+                table.setItem(i, 1, QTableWidgetItem(f"{self.values_set[i]}"))
+        except Exception as e:
+            self.show_error(e)
     # @pyqtSlot()
 
     # reaing contiunous block
     def start_reading(self):
-        self.sr = 2 if isinstance(self.samplingRate.text(), str) else int(self.samplingRate.text())  # how many second read again
-        self.samplingRate.setText(QtCore.QCoreApplication.translate("MainWindow", f'{self.sr}'))
-        self.reading = True
-        self.update_tracking_table()
-        self.set_led_on(1, 'green')
+        try:
+            self.sr = 2 if isinstance(self.samplingRate.text(), str) else int(self.samplingRate.text())  # how many second read again
+            self.samplingRate.setText(QtCore.QCoreApplication.translate("MainWindow", f'{self.sr}'))
+            self.reading = True
+            self.update_tracking_table()
+            self.set_led_on(1, 'green')
+        except Exception as e:
+            self.show_error(e)
 
     def stop_reading(self):
-        self.set_led_on(1, 'red')
-        self.reading = False
-        # self.thread.stop()
+        try:
+            self.set_led_on(1, 'red')
+            self.reading = False
+            # self.thread.stop()
+        except Exception as e:
+            self.show_error(e)
 
     # check updateValue.csv file and write updated value to plc continuously
     def start_writing(self):
@@ -94,78 +117,94 @@ class ModbusApp(Ui_MainWindow):
 
     # control tracking table
     def init_tracking_table(self):
-        _translate = QtCore.QCoreApplication.translate
-        self.read_csv_data('trackdevice')
-        table = self.trackingTable
-        # update name and type of tracking params
-        for i in range(len(self.name_track)):
-            table.verticalHeaderItem(i).setText(_translate("MainWindow", f"{self.name_track[i]}"))  # set name
-            table.setItem(i, 0, QTableWidgetItem(f"{self.type_track[i]}"))
-        print('init tracking table done')
+        try:
+            _translate = QtCore.QCoreApplication.translate
+            self.read_csv_data('trackdevice')
+            table = self.trackingTable
+            # update name and type of tracking params
+            for i in range(len(self.name_track)):
+                table.verticalHeaderItem(i).setText(_translate("MainWindow", f"{self.name_track[i]}"))  # set name
+                table.setItem(i, 0, QTableWidgetItem(f"{self.type_track[i]}"))
+            print('init tracking table done')
+        except Exception as e:
+            self.show_error(e)
 
     def update_tracking_table(self):
-        table = self.trackingTable
-        # read value from plc and update tracking values
-        for i in range(len(self.name_track)):
-            idx = int(self.address_track[i])
-            values = self.read_from_PLC(self.type_track[i], idx)
-            table.setItem(i, 1, QTableWidgetItem(f"{values}"))
-        pass
+        try:
+            table = self.trackingTable
+            # read value from plc and update tracking values
+            for i in range(len(self.name_track)):
+                idx = int(self.address_track[i])
+                values = self.read_from_PLC(self.type_track[i], idx)
+                table.setItem(i, 1, QTableWidgetItem(f"{values}"))
+        except Exception as e:
+            self.show_error(e)
 
     # reading and writing to PLC
 
     def write_to_PLC(self):
-        # self.update_set_value()
-        plc = ModbusClient(f'COM{self.com_set}')
-        if not plc.is_connected():
-            plc.connect()
-        for v, a, t in zip(self.values_set, self.address_set, self.type_set):
-            if t == 'coil':
-                v = 1 if int(v) != 0 else 0
-                plc.write_single_coil(a, v)
-            if t == 'reg':
-                v = int(v)
-                plc.write_single_register(a, v)
-        print("write done")
+        try:
+            # self.update_set_value()
+            plc = ModbusClient(f'COM{self.com_set}')
+            if not plc.is_connected():
+                plc.connect()
+            for v, a, t in zip(self.values_set, self.address_set, self.type_set):
+                if t == 'coil':
+                    v = 1 if int(v) != 0 else 0
+                    plc.write_single_coil(a, v)
+                if t == 'reg':
+                    v = int(v)
+                    plc.write_single_register(a, v)
+            print("write done")
+        except Exception as e:
+            self.show_error(e)
 
     def read_from_PLC(self, type_, address):
-        plc = ModbusClient(f'COM{self.com_set}')
-        if not plc.is_connected():
-            plc.connect()
-        if type_.strip() == 'hr':
-            return plc.read_holdingregisters(address, 1)
-
-        if type_.strip() == 'ir':
-            return plc.read_inputregisters(address, 1)
-
-        if type_.strip() == 'coil':
-            return plc.read_coils(address, 1)
-
-    # connect block
-    def connect_app(self):
-        self.com_set = self.spinBox.value()
-        self.baudrate_set = self.comboBox.currentText()
         try:
             plc = ModbusClient(f'COM{self.com_set}')
             if not plc.is_connected():
                 plc.connect()
-            print(plc.read_coils(0, 1))
-            self.connected = True
+            if type_.strip() == 'hr':
+                return plc.read_holdingregisters(address, 1)
+
+            if type_.strip() == 'ir':
+                return plc.read_inputregisters(address, 1)
+
+            if type_.strip() == 'coil':
+                return plc.read_coils(address, 1)
         except Exception as e:
-            print(e)
-        print(self.com_set, self.baudrate_set)
-        if self.connected:
-            self.connection_status.setStyleSheet("background-color: rgb(0, 170, 0)")
-        pass
+            self.show_error(e)
+
+    # connect block
+    def connect_app(self):
+        try:
+            self.com_set = self.spinBox.value()
+            self.baudrate_set = self.comboBox.currentText()
+            try:
+                plc = ModbusClient(f'COM{self.com_set}')
+                if not plc.is_connected():
+                    plc.connect()
+                print(plc.read_coils(0, 1))
+                self.connected = True
+            except Exception as e:
+                print(e)
+            print(self.com_set, self.baudrate_set)
+            if self.connected:
+                self.connection_status.setStyleSheet("background-color: rgb(0, 170, 0)")
+        except Exception as e:
+            self.show_error(e)
 
     # display led block
     def set_led_on(self, led_num, color):
-        led_list = [self.led1, self.led2, self.led3, self.led4, self.led5, self.led6, self.led7, self.led8, self.led9, self.led10]
-        if isinstance(led_num, list):
-            for idx in led_num:
-                led_list[idx - 1].setStyleSheet(f"background-color: {color}")
-        elif isinstance(led_num, int):
-            led_list[led_num - 1].setStyleSheet(f"background-color: {color}")
+        try:
+            led_list = [self.led1, self.led2, self.led3, self.led4, self.led5, self.led6, self.led7, self.led8, self.led9, self.led10]
+            if isinstance(led_num, list):
+                for idx in led_num:
+                    led_list[idx - 1].setStyleSheet(f"background-color: {color}")
+            elif isinstance(led_num, int):
+                led_list[led_num - 1].setStyleSheet(f"background-color: {color}")
+        except Exception as e:
+            self.show_error(e)
 
     def set_random(self):
         k = random.sample(range(2, 10), 5)
